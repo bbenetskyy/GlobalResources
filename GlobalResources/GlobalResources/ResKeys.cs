a2b5eb63b3cbd112986c48fdec5b31972f38a848
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2016-09-30 15:11:49 +0200

    Resource library with uk-UA localization only Available in nuget: Install-Package GlobalResources

---
+++ GlobalResources/GlobalResources/ResKeys.cs
@@ -0,0 +1,142 @@
+﻿namespace GlobalResources
+{
+    public static class ResKeys
+    {
+        public const string FillCaptchaAndPress = "FillCaptchaAndPress";
+        public const string ServerError = "ServerError";
+
+        public const string ValidationError_Message_FirstPart =
+            "ValidationError_Message_FirstPart";
+
+        public const string ValidationError_Message_SecondPart =
+            "ValidationError_Message_SecondPart";
+
+        public const string ValidationError_Title = "ValidationError_Title";
+        public const string SearchResult = "SearchResult";
+        public const string ExceptionHanler_Text = "ExceptionHanler_Text";
+        public const string ExceptionHanler_Title = "ExceptionHanler_Title";
+        public const string colPeopleCount = "colPeopleCount";
+        public const string colChildsCount = "colChildsCount";
+        public const string colNumberOfReceipt = "colNumberOfReceipt";
+        public const string colEndPassportDate = "colEndPassportDate";
+        public const string colStatus = "colStatus";
+        public const string colName = "colName";
+        public const string colLastName = "colLastName";
+        public const string colBirthday = "colBirthday";
+        public const string colReturnData = "colReturnData";
+        public const string Nationality_Text = "Nationality_Text";
+        public const string colRegistryFom = "colRegistryFom";
+        public const string colRegistryTo = "colRegistryTo";
+        public const string colRegistryTime = "colRegistryTime";
+
+        public const string ValidationError_Message_NullText =
+            "ValidationError_Message_NullText";
+
+        public const string ButtonRegistry_Text = "ButtonRegistry_Text";
+        public const string Status_NullText = "Status_NullText";
+        public const string ServiceCenter_NullText = "ServiceCenter_NullText";
+        public const string Nationality_NullText = "Nationality_NullText";
+        public const string RegistryTime_NullText = "RegistryTime_NullText";
+        public const string VisaCategory_NullText = "VisaCategory_NullText";
+        public const string lblServiceCenter = "lblServiceCenter";
+        public const string lblVisaCategory = "lblVisaCategory";
+        public const string lblCancelGroup = "lblCancelGroup";
+        public const string lblCloseBrowser = "lblCloseBrowser";
+        public const string lblAvailabilityUrl = "lblAvailabilityUrl";
+        public const string lblEmail = "lblEmail";
+        public const string lblChooseProxies = "lblChooseProxies";
+        public const string ButtonCancelAction_Text = "ButtonCancelAction_Text";
+        public const string ToggleSwitch_OnText = "ToggleSwitch_OnText";
+        public const string ToggleSwitch_OffText = "ToggleSwitch_OffText";
+        public const string lblClientDataGroup = "lblClientDataGroup";
+
+        public const string BarButtonItemImport_Caption =
+            "BarButtonItemImport_Caption";
+
+        public const string BarButtonItemSetup_Caption =
+            "BarButtonItemSetup_Caption";
+
+        public const string ApplicationMenu_Caption = "ApplicationMenu_Caption";
+        public const string RepeatIfCrash_Text = "RepeatIfCrash_Text";
+        public const string Password_Text = "Password_Text";
+        public const string ButtonLoadProxies_Text = "ButtonLoadProxies_Text";
+        public const string ButtonOk_Text = "ButtonOk_Text";
+        public const string ButtonCancel_Text = "ButtonCancel_Text";
+        public const string OpenFileDialog_Title = "OpenFileDialog_Title";
+        public const string FillCaptchaAndComplete = "FillCaptchaAndComplete";
+        public const string DateIncorrect_Message = "DateIncorrect_Message";
+        public const string Key_Used = "Key_Used";
+        public const string KeyNotFound = "KeyNotFound";
+        public const string PageAvailable = "PageAvailable";
+        public const string PageNotAvailable = "PageNotAvailable";
+
+        public const string AvailabilityError_Message =
+            "AvailabilityError_Message";
+
+        public const string colRegistryFom_ValidationError =
+            "colRegistryFom_ValidationError";
+
+        public const string colRegistryTo_ValidationError =
+            "colRegistryTo_ValidationError";
+
+        public const string colRegistryTo_DataColumn_Name =
+            "colRegistryTo_DataColumn_Name";
+
+        public const string colRegistryFom_DataColumn_Name =
+            "colRegistryFom_DataColumn_Name";
+
+        public const string WebPage_StillNotLoaded = "WebPage_StillNotLoaded";
+        public const string WebPage_WillBeReloaded = "WebPage_WillBeReloaded";
+
+        public const string ValidationError_Message_Option_Email =
+            "ValidationError_Message_Option_Email";
+
+        public const string ValidationError_Message_Option_Password =
+            "ValidationError_Message_Option_Password";
+
+        public const string Application_Logic_Error = "Application_Logic_Error";
+
+        public const string Fill_Calendar_And_Captcha =
+            "Fill_Calendar_And_Captcha";
+
+        public const string Complete_Registration = "Complete_Registration";
+
+        public const string DateСorrect_Message = "DateСorrect_Message";
+
+        public const string NoDate_Message = "NoDate_Message";
+
+        public const string DateSelected_Message = "DateSelected_Message";
+
+        public const string ValidationError_Message_Customer =
+            "ValidationError_Message_Customer";
+
+        public const string AddNewBarButton_Caption = "AddNewBarButton_Caption";
+        public const string CancelBarButton_Caption = "CancelBarButton_Caption";
+        public const string CopyBarButton_Caption = "CopyBarButton_Caption";
+        public const string CustomerBarEdit_Caption = "CustomerBarEdit_Caption";
+
+        public const string CustomersBarButton_Caption =
+            "CustomersBarButton_Caption";
+
+        public const string DeleteAllBarButton_Caption =
+            "DeleteAllBarButton_Caption";
+
+        public const string DeleteBarButton_Caption = "DeleteBarButton_Caption";
+        public const string EditGroup_title = "EditGroup_title";
+        public const string EditPage_Title = "EditPage_Title";
+
+        public const string LicensesBarButton_Caption =
+            "LicensesBarButton_Caption";
+
+        public const string MainGroup_Title = "MainGroup_Title";
+        public const string MainPage_Title = "MainPage_Title";
+        public const string OperationsGroup_Title = "OperationsGroup_Title";
+
+        public const string RefreshBarButton_Caption =
+            "RefreshBarButton_Caption";
+
+        public const string SaveBarButton_Caption = "SaveBarButton_Caption";
+        public const string SkinGroup_Title = "SkinGroup_Title";
+        public const string SkinPage_Title = "SkinPage_Title";
+    }
+}
---
+++ GlobalResources/GlobalResources/ResManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Resources;
+using ToolsPortable;
+
+namespace GlobalResources
+{
+    public class ResManager : SortedList
+    {
+        private static ResManager _instance;
+        public static string NoDataSource = "__NO_DATA__";
+
+        public static ResManager Instance
+            => _instance ?? (_instance = new ResManager());
+
+        public static void RegisterResource(string sys,
+            ResourceManager mgr)
+        {
+            if (!Instance.ContainsKey(sys))
+                Instance.Add(sys,
+                    mgr);
+        }
+
+        public static void RemoveResource(string sys)
+        {
+            Instance.Remove(sys);
+        }
+
+        public static void ClearResources() => Instance.Clear();
+
+        public static string GetString(string code)
+        {
+            if (code == null)
+                return NoDataSource;
+            foreach (DictionaryEntry de in Instance)
+            {
+                var res = (de.Value as ResourceManager)?.GetString(code);
+                if (res.IsNotBlank())
+                    return res;
+            }
+            return NoDataSource;
+        }
+
+        public static string GetString(string sys,
+            string code)
+        {
+            if (!Instance.ContainsKey(sys))
+                return NoDataSource;
+
+            var res = (Instance[sys] as ResourceManager)?.GetString(code);
+            if (res.IsNotBlank())
+                return res;
+            return GetString(code);
+        }
+
+        public static string GetString(string sys,
+            string code,
+            params object[] sParams)
+        {
+            if (!Instance.ContainsKey(sys))
+                return NoDataSource;
+
+            var res = (Instance[sys] as ResourceManager)?.GetString(code);
+
+            if (res.IsBlank())
+                return NoDataSource;
+
+            if (sParams != null
+                && sParams.Length > 0)
+                // ReSharper disable once AssignNullToNotNullAttribute
+                return string.Format(res,
+                    sParams);
+
+            return NoDataSource;
+        }
+    }
+}
